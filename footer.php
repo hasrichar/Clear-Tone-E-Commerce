5619fd25a5524d35e21a023715d80fb5f66fcb99
has_richard <richardhsb03@gmail.com>
2023-12-26 11:55:11 +0700

    after fix final upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +62 895-6182-70159 </p>
+         <p> <i class="fas fa-phone"></i> +62 895-4211-78436 </p>
+         <p> <i class="fas fa-envelope"></i> KLP 5 </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Riau, Pekanbaru </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ by <span>KLP 5 CLR-TONE</span> </p>
+
+</section>
